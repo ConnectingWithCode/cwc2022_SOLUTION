699a1bff8d0f4cd286cb95c15d63038781a115e5
aidancmatthews <aidancmatthews@gmail.com>
2022-01-27 14:57:55 -0500

    Refactored CWC addition

--- CSSE290Winter20212022/Aidan/aidanaddit.py
+++ CSSE290Winter20212022/Aidan/aidanaddit.py
@@ -2,23 +2,24 @@ import rosegraphics as rg
 import math
 
 
-# I have reservations on whether the mathematical jargon used to inscribe the shapes may be confusing to
-# younger folks with less geometry experience. Also, could refactor to recursively inscribe circles to a given depth
+def second_point_calc(pt1, pt2):
+    outer_circle = math.sqrt(((pt2.x - pt1.x) * (pt2.x - pt1.x)) + ((pt2.y - pt1.y) * (pt2.y - pt1.y)))
+    outer_square = math.sqrt(2) * outer_circle
+    inner_circle = 0.5 * outer_square
+    inner_square = math.sqrt(2) * inner_circle
+    return [outer_circle, outer_square, inner_circle, inner_square]
 
-# The first click defines the center of the shape while the second click defines the radius of the outer circle
 
 def main():
     window = rg.RoseWindow(400, 600, title="Click to draw inscribed squares and circles")
     pt1 = window.get_next_mouse_click()
     while True:
         pt2 = window.get_next_mouse_click()
-        circle = rg.Circle(pt1, math.sqrt(((pt2.x - pt1.x) * (pt2.x - pt1.x)) + ((pt2.y - pt1.y) * (pt2.y - pt1.y))))
-        square = rg.Square(pt1, math.sqrt(2) * math.sqrt(
-            ((pt2.x - pt1.x) * (pt2.x - pt1.x)) + ((pt2.y - pt1.y) * (pt2.y - pt1.y))))
-        inner_circle = rg.Circle(pt1, 0.5 * math.sqrt(2) * math.sqrt(
-            ((pt2.x - pt1.x) * (pt2.x - pt1.x)) + ((pt2.y - pt1.y) * (pt2.y - pt1.y))))
-        inner_square = rg.Square(pt1, math.sqrt(2) * (0.5 * math.sqrt(2) * math.sqrt(
-            ((pt2.x - pt1.x) * (pt2.x - pt1.x)) + ((pt2.y - pt1.y) * (pt2.y - pt1.y)))))
+        second_points = second_point_calc(pt1, pt2)
+        circle = rg.Circle(pt1, second_points[0])
+        square = rg.Square(pt1, second_points[1])
+        inner_circle = rg.Circle(pt1, second_points[2])
+        inner_square = rg.Square(pt1, second_points[3])
         circle.attach_to(window)
         square.attach_to(window)
         inner_circle.attach_to(window)
